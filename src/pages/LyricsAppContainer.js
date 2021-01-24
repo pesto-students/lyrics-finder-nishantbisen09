12d7eecacebdb90fceaaadfe73dc08689c25e066
nishantbisen09 <nishantbisen09@gmail.com>
2021-01-24 23:21:50 +0530

    remove obsolete colsole logs

--- src/pages/LyricsAppContainer.js
+++ src/pages/LyricsAppContainer.js
@@ -14,7 +14,6 @@ class LyricsAppContainer extends Component {
   };
 
   render() {
-    console.log(this.props);
     return (
       <div className='app-container'>
         <div className='search-panel-container'>
