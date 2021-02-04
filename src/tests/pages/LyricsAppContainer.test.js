02313ae5787547f4a0a854e14a8e3d624d7c44b2
nishantbisen09 <nishantbisen09@gmail.com>
2021-02-04 17:08:35 +0530

    remove obsolete test case

--- src/tests/pages/LyricsAppContainer.test.js
+++ src/tests/pages/LyricsAppContainer.test.js
@@ -8,16 +8,4 @@ describe('LyricsAppContainer', () => {
     const component = mount(<LyricsAppContainer />);
     expect(component.find(SearchBar).exists()).toBeTruthy();
   });
-
-  xit('Should show a LyricDetailViewer component when isLyricView  is true', () => {
-    const component = mount(<LyricsAppContainer />);
-    component.instance().state = {
-      isLyricView: true,
-      currentLyrics: {
-        lyrics: 'hello',
-        artist: { name: 'arijit', picture_small: 'path' },
-      },
-    };
-   expect( component.find(LyricDetailViewer).length).toBe(1)
-  });
 });
