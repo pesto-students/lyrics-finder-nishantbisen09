7ca3a66645f4bac80555298f8f1956d5beb3b519
nishantbisen09 <nishantbisen09@gmail.com>
2021-02-09 01:55:20 +0530

    Add useEffect for image path change

--- src/components/resultCard/ResultCard.js
+++ src/components/resultCard/ResultCard.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { APP_MESSAGES } from '../../utility/strings';
 import './resultCard.css';
 import defaultArtistImage from '../../assets/images/album-art-placeholder.jpg';
 
 export const ResultCard = ({ artist, title }) => {
-  const [artistImage, setArtistImage] = useState(
-    artist && artist.picture_small ? artist.picture_small : defaultArtistImage
-  );
+  const [artistImage, setArtistImage] = useState(defaultArtistImage);
+
+  useEffect(() => {
+    setArtistImage(
+      artist && artist.picture_small ? artist.picture_small : defaultArtistImage
+    );
+  }, [artist]);
 
   return (
     <div className='lyric-card-container'>
@@ -21,7 +25,7 @@ export const ResultCard = ({ artist, title }) => {
               : APP_MESSAGES.artistNameUnavailable}
           </span>
         </div>
-        <div className='lyric-album-art' >
+        <div className='lyric-album-art'>
           <img
             src={artistImage}
             onError={() => setArtistImage(defaultArtistImage)}
